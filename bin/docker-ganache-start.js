6851a6956fe9e75dad30fa1d7082f9ef9372cfdb
michal.kloczko <michal.kloczko@tratif.com>
2022-10-20 17:10:15 +0200

    used an absolute path to the package docker-compose file

--- bin/docker-ganache-start.js
+++ bin/docker-ganache-start.js
@@ -7,6 +7,7 @@ const shell = require('shelljs');
 const tmpDirPath = path.resolve(path.join(__dirname, '../.tmp/'));
 const tmpDbPath = path.join(tmpDirPath, 'database');
 const savedDbPath = path.resolve(path.join(__dirname, '../database/'));
+const dockerComposeYmlPath = path.resolve(path.join(__dirname, '../docker-compose.yml'));
 
 shell.echo('Removing temporary ganache database...');
 shell.rm('-rf', tmpDirPath);
@@ -21,7 +22,7 @@ shell.cp('-R', savedDbPath, tmpDbPath);
 shell.echo('Behodler state ready.');
 
 shell.echo('Starting Behodler ganache dev env...');
-shell.exec('docker-compose up --remove-orphans -d ganache');
+shell.exec(`docker-compose -f ${dockerComposeYmlPath} up --remove-orphans -d ganache`);
 
 const child = spawn.sync('node', __filename, { stdio: 'inherit' });
 process.exit(child.signal || child.status);
