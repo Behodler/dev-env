4bccb66c3d3667c82a7bdc56c45562f256888fc5
michal.kloczko <michal.kloczko@tratif.com>
2022-10-21 11:27:37 +0200

    updated start script with some optimizations, .tmp dir is no longer removed when using stop script

--- bin/docker-ganache-start.js
+++ bin/docker-ganache-start.js
@@ -1,25 +1,42 @@
 #!/usr/bin/env node
 
 const path = require('path');
+const fs = require('fs');
 const spawn = require('cross-spawn');
 const shell = require('shelljs');
+const dirCompare = require('dir-compare');
 
 const tmpDirPath = path.resolve(path.join(__dirname, '../.tmp/'));
 const tmpDbPath = path.join(tmpDirPath, 'database');
 const savedDbPath = path.resolve(path.join(__dirname, '../database/'));
 const dockerComposeYmlPath = path.resolve(path.join(__dirname, '../docker-compose.yml'));
 
-shell.echo('Removing temporary ganache database...');
-shell.rm('-rf', tmpDirPath);
-shell.echo('Database removed.');
+if (!fs.existsSync(tmpDirPath)) {
+  shell.echo('Creating .tmp directory...');
+  shell.mkdir('-p', tmpDirPath);
 
-shell.echo('Restoring .tmp directory...');
-shell.mkdir('-p', tmpDirPath);
-shell.echo('.tmp directory created.');
+  shell.echo('Copying saved Behodler state to ganache temp db dir...');
+  shell.cp('-R', savedDbPath, tmpDbPath);
+  shell.echo('Behodler state ready.');
+} else {
+  shell.echo('Checking changes to saved db...');
 
-shell.echo('Copying saved Behodler state to ganache temp db dir...');
-shell.cp('-R', savedDbPath, tmpDbPath);
-shell.echo('Behodler state ready.');
+  const dbComparisonResult = dirCompare.compareSync(tmpDbPath, savedDbPath, {
+    compareSize: true,
+    compareContent: true,
+  });
+
+  if (!dbComparisonResult.same) {
+    shell.echo('Changes detected, removing temporary ganache database...');
+    shell.rm('-rf', tmpDbPath);
+
+    shell.echo('Copying saved Behodler state to ganache temp db dir...');
+    shell.cp('-R', savedDbPath, tmpDbPath);
+    shell.echo('Behodler state ready.');
+  } else {
+    shell.echo('No changes detected.');
+  }
+}
 
 shell.echo('Starting Behodler ganache dev env...');
 shell.exec(`docker-compose -f ${dockerComposeYmlPath} up --remove-orphans -d ganache`);
