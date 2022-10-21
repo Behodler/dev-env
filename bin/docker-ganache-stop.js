02f89a7f20625d963211debae0b79781c49337e4
michal.kloczko <michal.kloczko@tratif.com>
2022-10-21 13:42:52 +0200

    pointed to the right docker-compose file

--- bin/docker-ganache-stop.js
+++ bin/docker-ganache-stop.js
@@ -2,9 +2,11 @@
 
 const spawn = require('cross-spawn');
 const shell = require('shelljs');
+const path = require('path');
+const dockerComposeYmlPath = path.resolve(path.join(__dirname, '../docker-compose.yml'));
 
 shell.echo('Stopping Behodler ganache dev env...');
-shell.exec('docker-compose down');
+shell.exec(`docker-compose -f ${dockerComposeYmlPath} down`);
 
 const child = spawn.sync('node', __filename, { stdio: 'inherit' });
 process.exit(child.signal || child.status);
