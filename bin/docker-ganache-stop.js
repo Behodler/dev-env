6bc1a7170e88d2b6624c0c771416a96260f1b336
justin <justingoro@protonmail.com>
2022-10-22 22:47:45 +0200

    posix eaaccess resolution

--- bin/docker-ganache-stop.js
+++ bin/docker-ganache-stop.js
@@ -6,3 +6,7 @@ const dockerComposeYmlPath = path.resolve(path.join(__dirname, '../docker-compos
 
 shell.echo('Stopping Behodler ganache dev env...');
 shell.exec(`docker compose -f ${dockerComposeYmlPath} down`);
+
+shell.echo("Restoring file permissions for executing user")
+const tmpDirPath = path.resolve(path.join(__dirname, '../.tmp/'));
+shell.chmod("-R","704",tmpDirPath)
