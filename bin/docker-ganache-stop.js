53fba8b2efc8dddd2a22b948659b359a85b4f95d
michal.kloczko <michal.kloczko@tratif.com>
2022-10-24 09:10:59 +0200

    removed redundant const

--- bin/docker-ganache-stop.js
+++ bin/docker-ganache-stop.js
@@ -8,4 +8,3 @@ shell.echo('Stopping Behodler ganache dev env...');
 shell.exec(`docker compose -f ${dockerComposeYmlPath} down`);
 
 shell.echo("Restoring file permissions for executing user")
-const tmpDirPath = path.resolve(path.join(__dirname, '../.tmp/'));
